11a3f1bf6ab9076d2d264e9e90aae4a7cdfe209e
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-10 14:42:07 +0100

    fasho

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/Signin";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
 
 export default function app() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="projects" element={<Projectscts />} />
       </Routes>
     </>
   );
