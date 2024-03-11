e9737c473aed40705ca83d9dbf29e6c5f080ddbf
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-11 10:12:40 +0100

    going

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
 import Projects from "./Pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="projects" element={<Projects />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="projects" element={<Projects />} />
+        </Routes>
+      </div>
     </>
   );
 }
