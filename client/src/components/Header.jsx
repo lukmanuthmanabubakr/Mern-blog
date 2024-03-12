a9033d2363001b477a5d0c95633e7ff2673d79a3
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-12 01:57:15 +0100

    sharp

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,9 @@ import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-      <NavLink to="/">
-
+      <NavLink to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Legend's</span>
+          Blog
       </NavLink>
     </Navbar>
   )
