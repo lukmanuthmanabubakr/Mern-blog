269211ce523abef0349d9f2e74561e776419d7bb
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-12 01:46:38 +0100

    going

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,12 @@
 import { Navbar } from 'flowbite-react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <Navbar className='border-b-2'>Header</Navbar>
+    <Navbar className='border-b-2'>
+      <NavLink to="/">
+
+      </NavLink>
+    </Navbar>
   )
 }
