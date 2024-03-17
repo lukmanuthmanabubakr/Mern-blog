c6591a41aed06bbfbd39dc544b0f67008276dcf9
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-17 11:07:43 +0100

    email

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Please input A name"],
         unique: true,
+    },
+    email: {
+        
     }
 })
