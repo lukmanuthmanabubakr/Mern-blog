93388ed1e63021ffe767f69081271572c619e902
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-17 11:34:45 +0100

    Api is working fine

--- api/index.js
+++ api/index.js
@@ -18,3 +18,7 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.get('/test', (req, res) => {
+  res.json({message: `Api is working fine`})
+})
