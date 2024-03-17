ed3ec6c14dfe19f6d5208405f3cd5b8b61871c82
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-17 11:12:24 +0100

    going

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please input A name"],
-        unique: true,
-    },
-    email: {
-        
-    }
-})
+  username: {
+    type: String,
+    required: [true, "Please input A name"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Input an Email"],
+    unique: true,
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
+  },
+});
