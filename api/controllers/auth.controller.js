d531480a027cb5175fd7e00089ff69b0a6c6d1e1
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-19 21:30:29 +0100

    utils

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -27,6 +27,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Signup successful");
   } catch (error) {
-    res.status(500).json({message: error.message});
+    next(error);
   }
 };
