1b3b0f2efc10c11c78bf356ccd504641059d5c34
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-20 21:18:34 +0100

    instead add a div

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,7 +1,9 @@
 export default function SignUp() {
   return (
-    <div className="min-h-screen mt-50">
+    <div className="min-h-screen mt-20">
+      <div className="">
 
+      </div>
     </div>
   )
 }
