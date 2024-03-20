edfd35aa89bc2c7fdac56130d632426a6ee5e4aa
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-20 21:19:20 +0100

    left-side

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -2,7 +2,9 @@ export default function SignUp() {
   return (
     <div className="min-h-screen mt-20">
       <div className="">
+        <div className="">
 
+        </div>
       </div>
     </div>
   )
