433ba457992e50cac5d3315b2d7e7ab345714b09
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-21 20:35:02 +0100

    removal

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -8,7 +8,7 @@ export default function SignUp() {
         <div className="">
         <NavLink
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        className="font-semibold dark:text-white text-4xl"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Legends
