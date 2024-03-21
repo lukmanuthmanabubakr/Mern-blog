d7b743dfa77e237d35cd303378667b0741da414a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-21 20:37:08 +0100

    bold

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -8,7 +8,7 @@ export default function SignUp() {
         <div className="">
         <NavLink
         to="/"
-        className="font-semibold dark:text-white text-4xl"
+        className="font-bold dark:text-white text-4xl"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Legends
