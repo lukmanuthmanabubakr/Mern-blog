e8ea0e395a60994328c6865f41834ef7c154d75b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-21 20:41:47 +0100

    project

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -6,21 +6,19 @@ export default function SignUp() {
       <div className="">
         {/* Left Side */}
         <div className="">
-        <NavLink
-        to="/"
-        className="font-bold dark:text-white text-4xl"
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Legends
-        </span>
-        Blog
-      </NavLink>
+          <NavLink to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Legends
+            </span>
+            Blog
+          </NavLink>
+          <p>
+            This is a demo project, You can sign up with ur email and password Or with Your Google Account
+          </p>
         </div>
         {/* right Side */}
-        <div className="">
-
-        </div>
+        <div className=""></div>
       </div>
     </div>
-  )
+  );
 }
