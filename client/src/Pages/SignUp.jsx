f598dd9c55b034ffec8f4de61698ad0afebb05c8
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-23 23:46:58 +0100

    lets continue

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -6,7 +6,7 @@ export default function SignUp() {
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
         {/* Left Side */}
-        <div className="">
+        <div className="flex-1">
           <NavLink to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Legends
@@ -20,7 +20,7 @@ export default function SignUp() {
         </div>
         {/* right Side */}
 
-        <div className="">
+        <div className="flex-1">
           <form className="flex flex-col gap-4">
             <div>
               <Label value="Your username" />
