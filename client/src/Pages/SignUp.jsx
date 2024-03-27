ddac214f2669466872364e9724c75cec92c7eb6e
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-27 19:04:21 +0100

    loading

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -7,21 +7,20 @@ export default function SignUp() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value.trim})
-   
-  }
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await fetch('/api/auth/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
     } catch (error) {}
-  }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
