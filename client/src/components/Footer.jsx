5de3d6cdffaa478b3b9f003157d5a8e1a93b8c87
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-29 10:29:48 +0100

    importer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Footer } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 export default function FooterCom() {
   return (
@@ -6,7 +7,15 @@ export default function FooterCom() {
       <div className="">
         <div className="">
           <div className="">
-
+            <NavLink
+              to="/"
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Legends
+              </span>
+              Blog
+            </NavLink>
           </div>
         </div>
       </div>
