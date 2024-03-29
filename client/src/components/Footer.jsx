a270e4292402a7ec7a3db68bfaafc30ef5ec7efe
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-29 16:05:15 +0100

    continua

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,14 +8,17 @@ export default function FooterCom() {
         <div className="">
           <div className="">
             <NavLink
-              to="/"
-              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+              to="/" 
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                 Legends
               </span>
               Blog
             </NavLink>
+          </div>
+          <div className="grid grid-cols-2 gap-3">
+
           </div>
         </div>
       </div>
