61158a3778ecc0e9b336e50fa1fdb3cd514db33c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-30 06:22:58 +0100

    auto center

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="">
+      <div className="w-full max-w-7xl mx-auto">
         <div className="">
           <div className="">
             <NavLink
