b7570a54e8510efe906d00cfb217f3ef3595de30
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-08 15:20:16 +0100

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,7 +4,7 @@ export const store = configureStore({
   reducer: {},
 })
 
-// // Infer the `RootSta
+// // Infer
 // export type RootState = ReturnType<typeof store.getState>
 // // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 // export type AppDispatch = typeof store.dispatch
