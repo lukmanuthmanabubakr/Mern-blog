7ccde194ab31430bd10223c969c50aceee1a9290
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-10 01:05:21 +0100

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -9,4 +9,4 @@ export const store = configureStore({
 
 
 // // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
+// export type AppDispatch = typeof store
