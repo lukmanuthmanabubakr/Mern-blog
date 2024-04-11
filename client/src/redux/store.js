03bb3798ca4f607d020ca217bfb79432b97b2844
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-11 09:41:21 +0100

    error solved

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,19 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
-})
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig);
 
 export const store = configureStore({
   reducer: {
