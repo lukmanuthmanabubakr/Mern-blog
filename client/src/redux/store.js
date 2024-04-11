4afcce2b37622efc2fd513b4a9b3d95747f5ba1c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-11 09:34:57 +0100

    Bismillah

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+
+const rootReducer = combineReducers({})
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
   },
-})
-
+});
 
 // // Inferred type: {posts:
