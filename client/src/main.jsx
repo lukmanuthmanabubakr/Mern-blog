5c55ce5eb4e0cbd2eb862954cf255f6f3eb9e543
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-11 21:51:01 +0100

    ok good

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
+ 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import persistStore from "redux-persist";
+import {persistStore} from "redux-persist";
 
 const rootReducer = combineReducers({
   user: userReducer,
