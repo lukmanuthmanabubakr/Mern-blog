b95b1ebb9a266af095aeff1b575d4a900b9bd2e0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-11 22:38:01 +0100

    famous

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,3 +1,9 @@
+import { Button } from 'flowbite-react'
+import { AiFillGoogleCircle } from "react-icons/ai";
+
 export default function OAuth() {
-  return <div>OAuth</div>;
+  return <Button type='button' gradientDuoTone='pinkToOrange' outline>
+    <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
+    Continue with Google
+  </Button>;
 }
