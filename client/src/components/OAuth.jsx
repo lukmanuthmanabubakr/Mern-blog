48c28e138b8a836323e44ab504c3dbcf0d94b40c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-11 22:39:31 +0100

    Now Lets make some magic

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,7 +2,7 @@ import { Button } from 'flowbite-react'
 import { AiFillGoogleCircle } from "react-icons/ai";
 
 export default function OAuth() {
-  return <Button type='button' gradientDuoTone='pinkToOrange' outline>
+  return <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
     <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
     Continue with Google
   </Button>;
