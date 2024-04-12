65302499bd3fbb4b77c11a16fac10e4e5fd5a475
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-12 18:55:07 +0100

    Lets navigate to

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,30 +1,39 @@
 import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
-    const auth = getAuth(app)
+  const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
-    const provider = new GoogleAuthProvider()
-    provider.setCustomParameters({ prompt: 'select_account' })
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
     try {
-        const resultsFromGoogle = await signInWithPopup(auth, provider)
+      const resultsFromGoogle = await signInWithPopup(auth, provider);
 
-        const res = await fetch('/api/auth/google', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            name: resultsFromGoogle.user.displayName,
-            email: resultsFromGoogle.user.email,
-            googlePhotoUrl: resultsFromGoogle.user.photoURL,
-          })
-        })
-        console.log(resultsFromGoogle);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/')
+      }
+      console.log(resultsFromGoogle);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-    
   };
 
   return (
