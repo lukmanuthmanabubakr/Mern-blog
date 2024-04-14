98af8ff6ed57ef708cd4e07c723360d084798ce6
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-14 21:26:49 +0100

    ok no issue

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -100,6 +100,14 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
     }
   } catch (error) {
     next(error);
