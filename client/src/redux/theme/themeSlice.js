f7c59506147798ad0376ce99dbbc3b4861383214
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-16 11:28:38 +0100

    going

--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -1,1 +1,13 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    theme: 'light'
+}
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        
+    }
+})
