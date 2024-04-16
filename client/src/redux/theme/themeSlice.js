bce8137e09543f0baa6ba309d6d97c85d1e3a18c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-16 11:30:08 +0100

    ehhh error

--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -1,13 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    theme: 'light'
-}
+  theme: "light",
+};
 
 const themeSlice = createSlice({
-    name: 'theme',
-    initialState,
-    reducers: {
-        
-    }
-})
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
+  },
+});
