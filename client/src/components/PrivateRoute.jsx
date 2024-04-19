feb027706f3b4fcffcccce8de24cb86ba111f1d7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-19 08:07:27 +0100

    Good morning

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,4 +1,7 @@
+import { useSelector } from "react-redux"
+
 export default function PrivateRoute() {
+    const { currentUser } = useSelector((state) => state.user)
   return (
     <div>PrivateRoute</div>
   )
