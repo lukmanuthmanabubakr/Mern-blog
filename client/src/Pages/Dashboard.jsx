38d72fe8596215264dab223dda18d442b0e4e89f
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-20 02:49:57 +0100

    let use useEffect

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    
+  })
   return <div></div>;
 }
