670761002da6189c2f2e9629cdeb8b9cccc3c311
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-20 03:13:42 +0100

    Let create the dashsidebar component

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 export default function Dashboard() {
-  const location = useLocation()
-  const [tab, setTab] = useState('')
+  const location = useLocation();
+  const [tab, setTab] = useState("");
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-  })
-  return <div></div>;
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div>
+      <div className="">
+          {/* Sidebar */}
+      </div>
+
+      {/* Profile... */}
+  </div>
+  );
 }
