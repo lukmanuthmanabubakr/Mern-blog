91f10076ee43be36d537f23e0d74ebf1fd09f1c6
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-21 08:56:11 +0100

    Good morning

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,16 @@
+import {Sidebar} from 'flowbite-react'
+import {HiUser} from 'react-icons/hi'
+
 export default function DashSidebar() {
   return (
-    <div>DashSidebar</div>
+    <Sidebar>
+        <Sidebar.Items>
+            <Sidebar.Group>
+                <Sidebar.Item active icon={HiUser}>
+                    
+                </Sidebar.Item>
+            </Sidebar.Group>
+        </Sidebar.Items>
+    </Sidebar>
   )
 }
