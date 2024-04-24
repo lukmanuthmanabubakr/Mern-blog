7a2e927ab035537133fbfbbe6b7b735b6d28a322
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-24 16:41:16 +0100

    update

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,18 @@
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
-  const { currentUser } = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div>
       <h1>profile</h1>
       <form>
-      <div className="">
-
-      </div>
-        <img src={currentUser.profilePicture} alt="user" className="rounded-full"/>
+        <div className="">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="rounded-full"
+          />
+        </div>
       </form>
     </div>
   );
