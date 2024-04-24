43aefed9d907c3e098f1eea6bc2a3019adf5ab06
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-24 16:51:29 +0100

    progress

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -3,14 +3,14 @@ import { useSelector } from "react-redux";
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>
+    <div className="max-w-lg mx-auto">
       <h1>profile</h1>
-      <form>
-        <div className="w-32 h-32">
+      <form className="flex flex-col">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
             src={currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full border-8  border-[lightgray]"
+            className="rounded-full w-full h-full object-cover border-8  border-[lightgray]"
           />
         </div>
       </form>
