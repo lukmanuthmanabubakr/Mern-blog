204080c867777a7c5b94cafbe292a85be795cf7b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-27 22:19:23 +0100

    change-images

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -6,6 +6,7 @@ export default function DashProfile() {
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
+      <input  type="file" accept="image/*"/>
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
             src={currentUser.profilePicture}
