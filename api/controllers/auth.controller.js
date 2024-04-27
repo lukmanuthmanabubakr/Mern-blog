dc1a5e0d1a15574651410d5534688856b30b16e3
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-27 14:39:42 +0100

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
   ) {
     next(errorHandler(400, "All fields are required"));
 
-    // return re0).json({ message
+    // return re0)
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
