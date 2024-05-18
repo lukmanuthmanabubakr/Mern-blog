6653b070eda9541631f03794c7dba8c451bf55bc
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-18 20:59:56 +0100

    dont give up

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,8 +5,8 @@ export default function DashProfile() {
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col cursor-pointer shadow-md overflow-hidden rounded-full">
-        <div className="w-32 h-32 self-center">
+      <form className="flex flex-col">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
             src={currentUser.profilePicture}
             alt="user"
