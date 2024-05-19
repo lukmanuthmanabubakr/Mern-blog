5f4450c67ba50fb654e9b50364bb2282fc599b79
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-19 09:48:27 +0100

    progress

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,32 +2,46 @@ import { useSelector } from "react-redux";
 import { Button, TextInput } from "flowbite-react";
 import { useState } from "react";
 
-
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
 
   const handleImageChange = (e) => {
-    setImageFile(e.target.files[0]);
-  }
-  console.log(imageFile);
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  console.log(imageFile, imageFileUrl);
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-      <input type="file" accept='image/*' onChange={handleImageChange}/>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full border-8 object-cover border-[lightgray]"
           />
         </div>
-        <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
-        <TextInput type="email" id="email" placeholder="email" defaultValue={currentUser.email}/>
-        <TextInput type="password" id="username" placeholder="password"/>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
 
-        <Button type="submit" gradientDuoTone='purpleToBlue' outline>
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
       </form>
