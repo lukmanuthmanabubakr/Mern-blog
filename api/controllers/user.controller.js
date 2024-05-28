b99260f6a1b73bc73bf0bbf6deabaf2263253d93
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-28 22:40:18 +0100

    ok, Lets progress

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
 export const test = (req, res) => {
     res.json({message: `Api is working fine`})
 }
+
+export const updateUser = async (req, res, next) => {
+    res.json({})
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get('/test', test)
+router.put('/update:userId', updateUser);
 
 // module.exports = router;
 export default router;
