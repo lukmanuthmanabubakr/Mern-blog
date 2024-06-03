0797830a1caf71ef67d5bda798cc2b42fc548efc
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-06-03 15:27:24 +0100

    ended with the backend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import {errorHandler} from '../utils/error.js'
 
 export const test = (req, res) => {
   res.json({ message: `Api is working fine` });
@@ -34,7 +36,16 @@ export const updateUser = async (req, res, next) => {
       );
     }
     try {
-        
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                profilePicture: req.body.profilePicture,
+                password: req.body.password,
+            },
+        }, {new: true});
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest)
     } catch (error) {
         next(error)
     }
