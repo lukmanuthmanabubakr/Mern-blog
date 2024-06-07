7e16d2b3ec54c3dbcef6fd2f344a41443499cd85
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-06-07 16:06:07 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Projects from "./Pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute copy";
+import CreatePost from "./Pages/CreatePost";
 
 export default function App() {
   return (
@@ -21,6 +23,9 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route path="/create-post" element={<CreatePost />} />
           </Route>
           <Route path="/projects" element={<Projects />} />
         </Routes>
