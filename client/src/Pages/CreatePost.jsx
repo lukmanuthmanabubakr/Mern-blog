167859b3e09e334dbc17d775941a59236031dca0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-06-08 19:06:12 +0100

    update

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,7 +1,7 @@
 export default function CreatePost() {
   return (
-    <div className='p-3 max-w-3xl mx-auto'>
-
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1>Create a Post</h1>
     </div>
   )
 }
