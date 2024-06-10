3d53f944a25bea01c783a53287cdef02dc05531e
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-06-10 12:06:08 +0100

    Good morning

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,7 +1,10 @@
 export default function CreatePost() {
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1>Create a Post</h1>
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
+      <form className="flex flex-col gap-4">
+        
+      </form>
     </div>
   )
 }
