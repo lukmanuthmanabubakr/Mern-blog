749c79b32b8e58e381912c86f457e936d1b7782d
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-06-12 15:41:40 +0100

    Done with callToaction

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -1,8 +1,8 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import CommentSection from '../components/CommentSection';
-import PostCard from '../components/PostCard';
+// import CommentSection from '../components/CommentSection';
+// import PostCard from '../components/PostCard';
 import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
@@ -88,13 +88,13 @@ export default function PostPage() {
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} />
+      {/* <CommentSection postId={post._id} /> */}
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-          {recentPosts &&
-            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+          {/* {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)} */}
         </div>
       </div>
     </main>
